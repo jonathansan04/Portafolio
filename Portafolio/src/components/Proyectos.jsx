95639d614a21571fa35082748ea7a20ce3058052
jonathansan04 <tatansantos4@gmail.com>
2024-11-19 09:32:32 -0500

    Proyectos

--- Portafolio/src/components/Proyectos.jsx
+++ Portafolio/src/components/Proyectos.jsx
@@ -5,10 +5,19 @@ import  React from 'react';
     <div>
       <h1>Proyectos</h1>
 
-      <h6>Estos son los proyectos que he realizado</h6>
-
+      <h6>Estos son algunos de los proyectos que he realizado</h6>
+      <div>
       <h5>Software de ganaderia</h5>
       <p>Proyecto de grado realizado en Reactjs, nodeJs, JavaScript y Firebase</p>
+      </div>
+
+      <div>
+      <h5>CRUD de bovinos </h5>
+      <p>Proyecto de CRUD de bovinos con almacenamiento de información con 2 versiones</p>
+      <p>Version 1: Realizado en Reactjs, nodeJS, JavaScript y MySQL</p>
+      <p>Version 2: Realizado en Reactjs, nodeJS, JavaScript y MongoDB</p>
+      </div>
+
       
     </div>
   );
